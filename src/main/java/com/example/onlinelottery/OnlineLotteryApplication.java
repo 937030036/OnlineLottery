817eb24135ce0049244a94c7d3869b8d7b38c115
暮云春树 0 <937030036@qq.com>
2022-05-15 16:32:50 +0800

    Springboot框架

---
+++ src/main/java/com/example/onlinelottery/OnlineLotteryApplication.java
@@ -0,0 +1,12 @@
+package com.example.onlinelottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class OnlineLotteryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineLotteryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlinelottery/OnlineLotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinelottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLotteryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
