5f1583d0422685fd61ea6702b8da7bf76a25d5ec
梓染若北 <1649510695@qq.com>
2022-05-20 17:36:21 +0800

    AwardMapper.java

--- src/main/java/com/example/onlinelottery/Dao/AwardMapper.java
+++ src/main/java/com/example/onlinelottery/Dao/AwardMapper.java
@@ -1,6 +1,7 @@
 package com.example.onlinelottery.Dao;
 
 import com.example.onlinelottery.Model.Award;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -10,4 +11,16 @@ import java.util.List;
 public interface AwardMapper {
     @Select("select grade,name,pic from #{id}_award")
     List<Award> getAwardListByUserMgr(String id);
+
+    @Select("select grade,name,pic from Award where grade=#{grade}")
+    Award getAwardByGrade(String grade);
+
+    @Select("select grade,name,pic from Award where name=#{name}")
+    Award getAwardByName(String name);
+
+    @Select("select grade,name,pic from Award where pic=#{pic}")
+    Award getAwardByPic(String pic);
+
+    @Insert("insert into Award(grade,name,pic) value(#{grade},#{name},#{pic})")
+    void addAward(Award award);
 }
